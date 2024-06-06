345c15772c1724f0b61ff111a94d3c13f98ec6ae
s27502 <s27502@pjwstk.edu.pl>
2024-06-06 11:39:20 +0200

    DbContext done

--- beb/beb/DbContext/AppDbContext.cs
+++ beb/beb/DbContext/AppDbContext.cs
@@ -1,6 +1,26 @@
-﻿namespace beb.DbContext;
+﻿using beb.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace beb.DbContext;
+
+public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Medicament> Medicaments { get; set; }
+    public DbSet<Prescription> Prescriptions { get; set; }
+    public DbSet<Prescription_Medicament> Prescription_Medicaments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Prescription_Medicament>()
+            .HasKey(pm => new { pm.IdMedicament, pm.IdPrescription });
+
+        modelBuilder.Entity<Doctor>().HasKey(d => d.IdDoctor);
+        modelBuilder.Entity<Patient>().HasKey(p => p.IdPatient);
+        modelBuilder.Entity<Medicament>().HasKey(m => m.IdMedicament);
+        modelBuilder.Entity<Prescription>().HasKey(pr => pr.IdPrescription);
+    }
 }
